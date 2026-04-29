cf0e3f5add7981e7edf7a8fd646874f5bfa19bb0
FasolyaRodion <Fasolyarodion@gmail.com>
2026-04-29 18:02:03 +0500

    collection29.ts - реализована функция calculateTotalProgress для расчета среднего прогресса активных пользователей

--- src/collection/collection29.ts
+++ src/collection/collection29.ts
@@ -5,5 +5,7 @@
 type User = {active: boolean, progress: number}
 
 export function calculateTotalProgress(users: User[]): number {
-
+	const filtered = users.filter(user => user.active && user.progress > 50);
+	if (filtered.length === 0) return 0;
+	return filtered.reduce((total, user) => total + user.progress, 0) / filtered.length;
 }
