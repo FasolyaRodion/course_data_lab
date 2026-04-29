8543a866e6d038b0675fb0d183592befd578124a
FasolyaRodion <Fasolyarodion@gmail.com>
2026-04-29 15:20:03 +0500

    collection11.ts - реализована функция groupBy для группировки массива объектов по заданному ключу

--- src/collection/collection11.ts
+++ src/collection/collection11.ts
@@ -3,5 +3,12 @@
 */
 
 export function groupBy<T extends Record<string, any>>(arr: T[], key: keyof T): Map<T[keyof T], T[]> {
-
+	const map = new Map<T[keyof T], T[]>();
+	for (const item of arr) {
+		const keyValue = item[key];
+		const group = map.get(keyValue) || [];
+		group.push(item);
+		map.set(keyValue, group);
+	}
+	return map;
 }
