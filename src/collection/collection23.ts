7b65341edee4f5e45c05c3a04f681c76c5a19c0f
FasolyaRodion <Fasolyarodion@gmail.com>
2026-04-29 17:03:47 +0500

    collection23.ts - реализована функция groupByCategory для группировки массива объектов

--- src/collection/collection23.ts
+++ src/collection/collection23.ts
@@ -3,7 +3,14 @@
 */
 
 export function groupByCategory<T extends Record<string, any>>(arr: T[], key: keyof T): Record<T[keyof T], T[]> {
-
+	return arr.reduce((acc, item) => {
+		const keyValue = item[key];
+		if (!acc[keyValue]) {
+			acc[keyValue] = [];
+		}
+		acc[keyValue].push(item);
+		return acc;
+	}, {} as Record<T[keyof T], T[]>);
 }
 
 
