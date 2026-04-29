59491f5b1c99d0e576e5c104bf144d6965a3041e
FasolyaRodion <Fasolyarodion@gmail.com>
2026-04-29 17:30:37 +0500

    collection26.ts - реализована функция groupProductsByPrice для группировки товаров по цене

--- src/collection/collection26.ts
+++ src/collection/collection26.ts
@@ -6,5 +6,14 @@ type Product = {name: string, price: number};
 type GroupedProducts = {cheap: Product[], medium: Product[], expensive: Product[]};
 
 export function groupProductsByPrice(products: Product[]): GroupedProducts {
-
+	return products.reduce((acc, product) => {
+		if (product.price < 1000) {
+			acc.cheap.push(product);
+		} else if (product.price <= 5000) {
+			acc.medium.push(product);
+		} else {
+			acc.expensive.push(product);
+		}
+		return acc;
+	}, { cheap: [], medium: [], expensive: [] } as GroupedProducts);
 }
