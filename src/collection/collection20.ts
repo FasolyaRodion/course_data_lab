6ca467e655fcc2ab9ba7df58bee68c5f104bc839
FasolyaRodion <Fasolyarodion@gmail.com>
2026-04-29 16:28:41 +0500

    collection20.ts - реализована функция groupUnfinishedHighPriorityTasks для группировки незавершенных приоритетных задач

--- src/collection/collection20.ts
+++ src/collection/collection20.ts
@@ -5,7 +5,15 @@
 export type Task = { category: string; priority: string; completed: boolean };
 
 export function groupUnfinishedHighPriorityTasks(tasks: Task[]): Map<string, Task[]> {
-
+	const map = new Map<string, Task[]>();
+	tasks
+		.filter(task => !task.completed && task.priority.toLowerCase() === 'high')
+		.forEach(task => {
+			const group = map.get(task.category) || [];
+			group.push(task);
+			map.set(task.category, group);
+		});
+	return map;
 }
 
 
