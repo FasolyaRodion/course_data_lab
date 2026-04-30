14576e11d719d9ee63f2f5f0c95dc279c564ca76
FasolyaRodion <Fasolyarodion@gmail.com>
2026-04-30 22:21:33 +0500

    serial06.ts - добавлен декоратор @Type для опционального поля manager в классе Employee

--- src/serial/serial06.ts
+++ src/serial/serial06.ts
@@ -2,10 +2,13 @@
 	Настройте класс так, чтобы опциональное поле manager восстанавливалось как User или оставалось null.
 */
 
+import "reflect-metadata";
+import { Type } from "class-transformer";
 import { User } from "./user";
 
 export class Employee {
   name: string;
+  @Type(() => User)
   manager: User | null; // Требует декоратора для типа
 
   constructor(name: string, manager: User | null) {
