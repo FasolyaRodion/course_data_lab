a60e3562339117bc407bb5d7193db18d88357ca8
FasolyaRodion <Fasolyarodion@gmail.com>
2026-04-30 22:06:11 +0500

    serial05.ts - добавлены декораторы @Type для преобразования Date и вложенного User

--- src/serial/serial05.ts
+++ src/serial/serial05.ts
@@ -2,10 +2,14 @@
 	Добавьте декоратор для преобразования строки в объект Date.
 */
 
+import "reflect-metadata";
+import { Type } from "class-transformer";
 import { User } from "./user";
 
 export class Session {
+  @Type(() => User)
   user: User; // Требует декоратора
+  @Type(() => Date)
   loginAt: Date; // Требует декоратора Date
 
   constructor(user: User, loginAt: Date) {
