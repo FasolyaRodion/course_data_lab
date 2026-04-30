bf36b2c8d5f8e944877a365f10e3b0501da12541
FasolyaRodion <Fasolyarodion@gmail.com>
2026-04-30 21:35:12 +0500

    serial01.ts - добавлен декоратор @Type для корректной десериализации вложенного объекта User

--- src/serial/serial01.ts
+++ src/serial/serial01.ts
@@ -2,11 +2,15 @@
 	Дан класс Order и сериализованные данные. Добавьте правильный декоратор к полю customer, чтобы метод getOrderInfo() работал.
 */
 
+import "reflect-metadata";
+import { Type } from "class-transformer";
 import { User } from "./user";
 
 export class Order {
   id: number;
   product: string;
+
+  @Type(() => User)
   customer: User; // Требует декоратора
 
   constructor(
