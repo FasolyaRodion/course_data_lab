61476163ba550c7465f124856454835320b0fa8a
FasolyaRodion <Fasolyarodion@gmail.com>
2026-04-30 21:44:35 +0500

    serial02.ts - добавлен декоратор @Type для корректной десериализации массива объектов User

--- src/serial/serial02.ts
+++ src/serial/serial02.ts
@@ -2,10 +2,14 @@
 	Модифицируйте класс Team, чтобы массив members корректно восстанавливался как массив объектов User.
 */
 
+import "reflect-metadata";
+import { Type } from "class-transformer";
 import { User } from "./user";
 
 export class Team {
   name: string;
+
+  @Type(() => User)
   members: User[]; // Требует декоратора
   
   constructor(name: string, members: User[]) {
