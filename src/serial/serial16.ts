2ad9d1a8117cf108c18bf0245c4e93a02cabeacc
FasolyaRodion <Fasolyarodion@gmail.com>
2026-05-01 13:52:22 +0500

    serial16.ts - реализован XPath запрос для поиска всех элементов с атрибутом id

--- src/serial/serial16.ts
+++ src/serial/serial16.ts
@@ -5,7 +5,7 @@
 import { select } from 'xpath';
 
 export function findElementsWithIdAttribute(doc: Document): Node[] {
-   const query = "" 	// Тут нужно написать XPath запрос
+   const query = "//*[@id]"
   return select(query, doc) as Node[];
 }
 
