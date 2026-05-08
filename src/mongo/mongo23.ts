7bc671c6fb65076ddbea653e1dc08247e9df58bb
FasolyaRodion <Fasolyarodion@gmail.com>
2026-05-08 20:30:39 +0500

    mongo23.ts - Реализована функция get_orders_with_users для объединения заказов с информацией о пользователях

--- src/mongo/mongo23.ts
+++ src/mongo/mongo23.ts
@@ -36,8 +36,24 @@ export interface OrderWithUser {
 export async function get_orders_with_users(db: Db): Promise<OrderWithUser[]> {
     // TODO: Объединить заказы с информацией о пользователях
 	return await db.collection("orders").aggregate([
-
-    ]).toArray() as OrderWithUser[]
+		{
+			$lookup: {
+				from: "users",
+				localField: "userId",
+				foreignField: "_id",
+				as: "userInfo"
+			}
+		},
+		{
+			$project: {
+				_id: 1,
+				userId: 1,
+				product: 1,
+				amount: 1,
+				userInfo: { name: 1, email: 1 }
+			}
+		}
+	]).toArray() as OrderWithUser[]
 }
 
 
