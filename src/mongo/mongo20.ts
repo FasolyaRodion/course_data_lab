7775c9c0740851e65356328d6a31675ac85de0df
FasolyaRodion <Fasolyarodion@gmail.com>
2026-05-08 19:06:09 +0500

    mongo20.ts - Реализована функция get_popular_tags для поиска популярных тегов с агрегацией и сортировкой по количеству постов

--- src/mongo/mongo20.ts
+++ src/mongo/mongo20.ts
@@ -20,6 +20,12 @@ export interface PopularTag {
 export async function get_popular_tags(db: Db): Promise<PopularTag[]> {
     // TODO: Найти самые популярные теги (по количеству постов и общему количеству просмотров)
     return await db.collection("posts").aggregate([
-
+        { $unwind: "$tags" },
+        { $group: {
+            _id: "$tags",
+            postCount: { $sum: 1 },
+            totalViews: { $sum: "$views" }
+        } },
+        { $sort: { postCount: -1 } }
     ]).toArray() as PopularTag[]
 }
