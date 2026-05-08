494325acd95c24c739c4a6b2d3052c48d5ffb306
FasolyaRodion <Fasolyarodion@gmail.com>
2026-05-08 15:05:36 +0500

    mongo02.ts - Реализована функция find_students_by_grade для поиска студентов по классу

--- src/mongo/mongo02.ts
+++ src/mongo/mongo02.ts
@@ -13,5 +13,5 @@ export class Student {
 
 export async function find_students_by_grade(db: Db, grade: string): Promise<Student[]> {
     // TODO: Найти всех студентов с указанным классом
-	return db.collection("students")
+	return await db.collection("students").find({ grade }).toArray() as Student[]
 }
