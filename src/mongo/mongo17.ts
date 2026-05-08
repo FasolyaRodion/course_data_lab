f57641f350b08fdc61a573dedb94992757655191
FasolyaRodion <Fasolyarodion@gmail.com>
2026-05-08 18:20:59 +0500

    mongo17.ts - Реализована функция get_top_selling_products для поиска топ продуктов по продажам

--- src/mongo/mongo17.ts
+++ src/mongo/mongo17.ts
@@ -19,8 +19,10 @@ export interface TopProduct {
 
 export async function get_top_selling_products(db: Db, limit: number): Promise<TopProduct[]> {
     // TODO: Найти топ-N продуктов по количеству продаж
-	return  await db.collection("products").aggregate([
-
+	return await db.collection("products").aggregate([
+		{ $sort: { sales: -1 } },
+		{ $limit: limit },
+		{ $project: { name: 1, sales: 1, price: 1 } }
 	]).toArray() as TopProduct[]
 }
 
