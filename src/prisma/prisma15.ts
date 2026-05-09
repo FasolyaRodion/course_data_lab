f7b602d0b24f6cf461823aa76dbd07ada8cd6216
FasolyaRodion <Fasolyarodion@gmail.com>
2026-05-10 00:15:58 +0500

    prisma15.ts - Реализована функция update_low_grades для обновления оценок ниже 3 до 3 с использованием updateMany, возвращающая количество обновленных записей

--- src/prisma/prisma15.ts
+++ src/prisma/prisma15.ts
@@ -1,7 +1,12 @@
 import { prisma } from './prisma_init'
 
 export async function update_low_grades() {
-    // TODO: Обновить все оценки ниже 3 на значение 3
-    // Использовать один запрос updateMany средствами Prisma
-    // Вернуть количество обновленных записей
+  // TODO: Обновить все оценки ниже 3 на значение 3
+  // Использовать один запрос updateMany средствами Prisma
+  // Вернуть количество обновленных записей
+  const result = await prisma.grade.updateMany({
+    where: { grade: { lt: 3 } },
+    data: { grade: 3 }
+  })
+  return result.count
 }
