56d4568625209209e37abb625a4f776a8be07eaf
FasolyaRodion <Fasolyarodion@gmail.com>
2026-05-10 01:21:27 +0500

    prisma17.ts - Реализована функция delete_courses_without_grades для удаления курсов без оценок с использованием deleteMany, возвращающая количество удаленных записей

--- src/prisma/prisma17.ts
+++ src/prisma/prisma17.ts
@@ -1,7 +1,15 @@
 import { prisma } from './prisma_init'
 
 export async function delete_courses_without_grades() {
-    // TODO: Удалить все курсы, у которых нет ни одной оценки
-    // Использовать один запрос deleteMany средствами Prisma
-    // Вернуть количество удаленных курсов
+  // TODO: Удалить все курсы, у которых нет ни одной оценки
+  // Использовать один запрос deleteMany средствами Prisma
+  // Вернуть количество удаленных курсов
+  const result = await prisma.course.deleteMany({
+    where: {
+      grades: {
+        none: {}
+      }
+    }
+  })
+  return result.count
 }
