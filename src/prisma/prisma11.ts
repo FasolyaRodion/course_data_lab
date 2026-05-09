78845270dddef02f616bc1b70f1a2b92d4fefbd5
FasolyaRodion <Fasolyarodion@gmail.com>
2026-05-09 18:36:11 +0500

    prisma11.ts- Реализована функция update_students_email_domain для обновления домена в email студентах

--- src/prisma/prisma11.ts
+++ src/prisma/prisma11.ts
@@ -1,8 +1,18 @@
 import { prisma } from './prisma_init'
 
-export async function update_students_email_domain(oldDomain: string, newDomain: string) {
-    // TODO: Обновить email у всех студентов, у которых email заканчивается на oldDomain
-    // Заменить домен на newDomain
-    // Вернуть количество обновленных записей
-	// Допускается обновление email с помощью простого цикла с запросами в теле цикла
+export async function update_students_email_domain(oldDomain: string, newDomain: string): Promise<number> {
+    const students = await prisma.student.findMany({
+        include: { person: true },
+        where: { person: { email: { endsWith: oldDomain } } }
+    });
+    let count = 0;
+    for (const student of students) {
+        const newEmail = student.person.email.replace(oldDomain, newDomain);
+        await prisma.student.update({
+            where: { id: student.id },
+            data: { person: { update: { email: newEmail } } }
+        });
+        count++;
+    }
+    return count;
 }
