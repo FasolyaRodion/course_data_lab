4a771b0a64ccf3cd48f60ed67f368ca306cd3072
FasolyaRodion <Fasolyarodion@gmail.com>
2026-05-09 17:02:17 +0500

    prisma09.ts - Реализована функция delete_students_without_grades с удалением студентов без оценок и возвратом количества удаленных записей

--- src/prisma/prisma09.ts
+++ src/prisma/prisma09.ts
@@ -1,6 +1,8 @@
 import { prisma } from './prisma_init'
 
 export async function delete_students_without_grades() {
-    // TODO: Удалить всех студентов, у которых нет ни одной оценки
-    // Вернуть количество удаленных студентов
+  // TODO: Удалить всех студентов, у которых нет ни одной оценки
+  // Вернуть количество удаленных студентов
+  const result = await prisma.student.deleteMany({ where: { grades: { none: {} } } });
+  return result.count;
 }
