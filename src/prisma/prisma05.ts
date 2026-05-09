dab95a714c751a2e121defabad1c2cf021636986
FasolyaRodion <Fasolyarodion@gmail.com>
2026-05-09 16:19:31 +0500

    prisma05.ts - Реализована функция find_courses_by_title_paginated для поиска курсов по подстроке в названии с пагинацией и сортировкой по дате создания

--- src/prisma/prisma05.ts
+++ src/prisma/prisma05.ts
@@ -1,11 +1,23 @@
-import { prisma } from './prisma_init'
+import { prisma } from "./prisma_init";
 
 export async function find_courses_by_title_paginated(
     titlePattern: string, 
     skip: number, 
     take: number
 ) {
-    // TODO: Найти курсы, в названии которых содержится указанная подстрока
-    // Применить пагинацию (skip, take) и отсортировать по дате создания (новые первыми)
-    // Вернуть массив курсов
+  // TODO: Найти курсы, в названии которых содержится указанная подстрока
+  // Применить пагинацию (skip, take) и отсортировать по дате создания (новые первыми)
+  // Вернуть массив курсов
+  return await prisma.course.findMany({
+    where: {
+      title: {
+        contains: titlePattern,
+      },
+    },
+    skip,
+    take,
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 }
