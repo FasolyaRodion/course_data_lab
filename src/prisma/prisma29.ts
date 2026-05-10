04a4126db5512894668e501a073fd87c191762fc
FasolyaRodion <Fasolyarodion@gmail.com>
2026-05-10 15:58:16 +0500

    prisma29.ts - Реализована функция find_students_with_same_names для поиска студентов с одинаковыми именами

--- src/prisma/prisma29.ts
+++ src/prisma/prisma29.ts
@@ -1,7 +1,26 @@
 import { prisma } from './prisma_init'
 
 export async function find_students_with_same_names() {
-    // TODO: Найти студентов с одинаковыми именами
-    // Использовать обработку в TypeScript
-    // Вернуть массив групп студентов с одинаковыми именами
+  // TODO: Найти студентов с одинаковыми именами
+  // Использовать обработку в TypeScript
+  // Вернуть массив групп студентов с одинаковыми именами
+  const students = await prisma.student.findMany({
+    include: { person: true }
+  });
+
+  const groups = new Map<string, any[]>();
+  for (const s of students) {
+    const name = s.person.name;
+    const group = groups.get(name) ?? [];
+    group.push(s);
+    groups.set(name, group);
+  }
+
+  const result: any[][] = [];
+  for (const [name, group] of groups.entries()) {
+    if (group.length > 1) {
+      result.push(group);
+    }
+  }
+  return result;
 }
