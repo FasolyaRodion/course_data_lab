fc30524dfbbd53c3aead565691aac41490eda233
FasolyaRodion <Fasolyarodion@gmail.com>
2026-05-10 14:13:19 +0500

    prisma25.ts - Реализована функция update_courses_without_grades_description для обновления описания курсов без оценок

--- src/prisma/prisma25.ts
+++ src/prisma/prisma25.ts
@@ -1,7 +1,18 @@
 import { prisma } from './prisma_init'
 
 export async function update_courses_without_grades_description() {
-    // TODO: Обновить описание курсов без оценок на "Нет оценок"
-    // Использовать один запрос updateMany средствами Prisma
-    // Вернуть количество обновленных курсов
+  // TODO: Обновить описание курсов без оценок на "Нет оценок"
+  // Использовать один запрос updateMany средствами Prisma
+  // Вернуть количество обновленных курсов
+  const result = await prisma.course.updateMany({
+    where: {
+      grades: {
+        none: {}
+      }
+    },
+    data: {
+      description: 'Нет оценок'
+    }
+  })
+  return result.count
 }
