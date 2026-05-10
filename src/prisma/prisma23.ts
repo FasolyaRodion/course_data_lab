982caf98c4aea68f40ec4bda8cefb3bdc7171f4c
FasolyaRodion <Fasolyarodion@gmail.com>
2026-05-10 12:58:35 +0500

    prisma23.ts - Реализована функция delete_students_without_person для удаления студентов без связанной записи в person

--- src/prisma/prisma23.ts
+++ src/prisma/prisma23.ts
@@ -1,7 +1,11 @@
 import { prisma } from './prisma_init'
 
 export async function delete_students_without_person() {
-    // TODO: Удалить студентов, у которых нет связанной записи в person
-    // Использовать фильтрацию средствами Prisma
-    // Вернуть количество удаленных записей
+  // TODO: Удалить студентов, у которых нет связанной записи в person
+  // Использовать фильтрацию средствами Prisma
+  // Вернуть количество удаленных записей
+  const result = await prisma.student.deleteMany({
+    where: { person: { is: null } }
+  });
+  return result.count;
 }
