6569fdb12d0860a41a2b759d7e718cd8fc9d054e
FasolyaRodion <Fasolyarodion@gmail.com>
2026-05-10 12:06:28 +0500

    prisma21.ts - Реализована функция find_students_without_courses для поиска студентов без оценок с включением информации о person

--- src/prisma/prisma21.ts
+++ src/prisma/prisma21.ts
@@ -1,7 +1,17 @@
 import { prisma } from './prisma_init'
 
 export async function find_students_without_courses() {
-    // TODO: Найти всех студентов, у которых нет ни одного курса (оценок)
-    // Использовать фильтрацию средствами Prisma (none)
-    // Вернуть массив студентов с информацией о person
+  // TODO: Найти всех студентов, у которых нет ни одного курса (оценок)
+  // Использовать фильтрацию средствами Prisma (none)
+  // Вернуть массив студентов с информацией о person
+  return await prisma.student.findMany({
+    where: {
+      grades: {
+        none: {}
+      }
+    },
+    include: {
+      person: true
+    }
+  });
 }
