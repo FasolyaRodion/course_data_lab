b87088bde0ae1a3d99f8e32dc58680ce6f6e82ab
FasolyaRodion <Fasolyarodion@gmail.com>
2026-05-10 15:22:16 +0500

    prisma28.ts - Реализована функция delete_students_before_date для удаления студентов, созданных до указанной даты, с использованием deleteMany, возвращающая количество удаленных записей

--- src/prisma/prisma28.ts
+++ src/prisma/prisma28.ts
@@ -1,7 +1,15 @@
 import { prisma } from './prisma_init'
 
 export async function delete_students_before_date(date: Date) {
-    // TODO: Удалить всех студентов, созданных до указанной даты
-    // Использовать один запрос deleteMany средствами Prisma
-    // Вернуть количество удаленных записей
+  // TODO: Удалить всех студентов, созданных до указанной даты
+  // Использовать один запрос deleteMany средствами Prisma
+  // Вернуть количество удаленных записей
+  const result = await prisma.student.deleteMany({
+    where: {
+      createdAt: {
+        lt: date
+      }
+    }
+  });
+  return result.count;
 }
